c29f3fe2d711e087f070b211d393021a6d693055
root <root@DESKTOP-S7GGDE6.localdomain>
2021-04-07 23:52:20 -0300

    Adicionando Resource Null

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ data "oci_identity_availability_domain" "ad" {
 }
 
 
-
 resource "oci_core_instance" "webserver1" {
   availability_domain = data.oci_identity_availability_domain.ad.name
   compartment_id      = var.compartment_ocid
@@ -25,8 +24,37 @@ resource "oci_core_instance" "webserver1" {
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
-    user_data           = "${base64encode(file("./setup.sh"))}"
+  }
+}
+
+
+resource "null_resource" "web-install" {
+
+  connection {
+    type        = "ssh"
+    user        = "ocp"
+    password    = ""
+    host        =  oci_core_instance.webserver1.assign_public_ip
+    private_key = var.ssh_public_key
+
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Aguarde finalizando a VM...'; sleep 1; done",
+      "sudo yum install httpd -y",
+      "sudo firewall-cmd --zone=public --add-service=http",
+      "sudo firewall-cmd --permanent --zone=public --add-service=http",
+      "cd /var/www/html/",
+      "sudo wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/u8j40_AS-7pRypC5boQT24w5QFPDTy-0j27BWBOfmsxbERTiuDtJQBIqfcsOH81F/n/idqfa2z2mift/b/bootcamp-oci/o/oci-f-handson-modulo-compute-website-files.zip",
+      "sudo sleep 5",
+      "sudo unzip oci-f-handson-modulo-compute-website-files.zip",
+      "sudo chown -R apache:apache /var/www/html",
+      "sudo rm -rf oci-f-handson-modulo-compute-website-files.zip",
+      "sudo systemctl start httpd",
+      "sudo sleep 2",
+      "sudo systemctl enable httpd",  
+    ]
+  }
 }
 
