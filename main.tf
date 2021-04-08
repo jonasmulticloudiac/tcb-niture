a0295010b8f6ac21ace12102e572a62d16e63472
root <root@DESKTOP-S7GGDE6.localdomain>
2021-04-07 22:04:41 -0300

    Adiconado Template data

--- main.tf
+++ main.tf
@@ -3,11 +3,18 @@ data "oci_identity_availability_domain" "ad" {
   ad_number      = var.ad_region_mapping[var.region]
 }
 
+
+data "template_file" "init" {
+  template = file("${path.module}/nituredata.tpl")
+}
+
 resource "oci_core_instance" "webserver1" {
   availability_domain = data.oci_identity_availability_domain.ad.name
   compartment_id      = var.compartment_ocid
   display_name        = "webserver1"
   shape               = "VM.Standard.E2.1.Micro"
+
+  user_data           = data.template_file.init.rendered
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.tcb_subnet.id
